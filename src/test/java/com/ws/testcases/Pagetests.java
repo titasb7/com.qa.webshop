e529b34d1caadf847545f7d8b980edc9e152a332
titas <titas@Titas-Laptop>
2024-02-26 11:55:52 -0700

    remember me test case added

--- src/test/java/com/ws/testcases/Pagetests.java
+++ src/test/java/com/ws/testcases/Pagetests.java
@@ -3,16 +3,10 @@ package com.ws.testcases;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import com.ws.base.Drivermanager;
-import com.ws.pages.Homepage;
-import com.ws.pages.loginpage;
-
-public class Pagetests  extends Drivermanager {
+public class Pagetests  extends BaseTest {
 	@Test(enabled = false)
 	public void testHomePageTitle() {
 		
-		InitApplication();
-		Homepage homepage =new Homepage();
 		
 	String 	title =homepage.getHomePageTitle();
 	Assert.assertTrue(title.contains("Demo Web Shop"));
@@ -22,46 +16,58 @@ public class Pagetests  extends Drivermanager {
 	driver.quit();
 	}
 	
-	@Test
+	@Test(enabled = false)
 	public void Testloginfunction() throws InterruptedException {
-		InitApplication();
-		Homepage homepage =new Homepage();
+		
 		homepage.clickLogin();
 		
-		loginpage Login=new loginpage();
+		
 	String loginpageTitle = Login.getloginpageTitle();
 		Assert.assertTrue(loginpageTitle.contains("Login"));
 		Login.enterEmail("selauto1@test.com");
 		Login.enterPassowrd("Pass@123");
-		
 	
 		Login.clickloginButton();
 		String 	title =homepage.getHomePageTitle();
 		Assert.assertTrue(title.contains("Demo Web Shop"));
 		homepage.clicklogoutlink();
-		driver.quit();
-		
-	
+
 			
 		}
+	@Test
 	public void testisForgotpasswordlinkPresent() {
-		InitApplication();
-		Homepage homepage =new Homepage();
+		
 		homepage.clickLogin();
 		
-		loginpage Login=new loginpage();
 	String loginpageTitle = Login.getloginpageTitle();
 		Assert.assertTrue(loginpageTitle.contains("Login"));
 		Login.enterEmail("selauto1@test.com");
 		Login.enterPassowrd("Pass@123");
 		boolean flag = Login.isForgotpasswordlinkPresent();
 		Assert.assertTrue(flag);
+		Login.clickloginButton();
+		String 	title =homepage.getHomePageTitle();
+		Assert.assertTrue(title.contains("Demo Web Shop"));
 		homepage.clicklogoutlink();
-		driver.quit();
-		
 		
 	}
-
+	@Test
+	public void trstclickremembermeButton() {
+		homepage.clickLogin();
+		
+		String loginpageTitle = Login.getloginpageTitle();
+			Assert.assertTrue(loginpageTitle.contains("Login"));
+			Login.enterEmail("selauto1@test.com");
+			Login.enterPassowrd("Pass@123");
+			boolean flag = Login.isForgotpasswordlinkPresent();
+			Assert.assertTrue(flag);
+			Login.clickremembermeButton();
+			Login.clickloginButton();
+			String 	title =homepage.getHomePageTitle();
+			Assert.assertTrue(title.contains("Demo Web Shop"));
+			homepage.clicklogoutlink();
+		
+	}
 
 }
 
