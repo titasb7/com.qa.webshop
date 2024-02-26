6b0bec63c31abda67f4a9457b1c81a483d1e7c8e
titas <titas@Titas-Laptop>
2024-02-26 11:27:22 -0700

    Forgotpassword test case added

--- src/test/java/com/ws/testcases/Pagetests.java
+++ src/test/java/com/ws/testcases/Pagetests.java
@@ -33,20 +33,34 @@ public class Pagetests  extends Drivermanager {
 		Assert.assertTrue(loginpageTitle.contains("Login"));
 		Login.enterEmail("selauto1@test.com");
 		Login.enterPassowrd("Pass@123");
-		boolean Forgotpasswordlink = Login.isForgotpasswordlinkPresent();
-	Assert.assertTrue(Forgotpasswordlink);
-	Thread.sleep(10);
-		Login.clickremembermeButton();
+		
+	
 		Login.clickloginButton();
 		String 	title =homepage.getHomePageTitle();
 		Assert.assertTrue(title.contains("Demo Web Shop"));
 		homepage.clicklogoutlink();
 		driver.quit();
 		
-		
+	
 			
 		}
+	public void testisForgotpasswordlinkPresent() {
+		InitApplication();
+		Homepage homepage =new Homepage();
+		homepage.clickLogin();
 		
+		loginpage Login=new loginpage();
+	String loginpageTitle = Login.getloginpageTitle();
+		Assert.assertTrue(loginpageTitle.contains("Login"));
+		Login.enterEmail("selauto1@test.com");
+		Login.enterPassowrd("Pass@123");
+		boolean flag = Login.isForgotpasswordlinkPresent();
+		Assert.assertTrue(flag);
+		homepage.clicklogoutlink();
+		driver.quit();
+		
+		
+	}
 
 
 }
